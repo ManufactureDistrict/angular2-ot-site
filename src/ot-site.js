b885728f1ece7ea0a51db71a7e1f6d786bd59bf9
Kara Erickson <kara.erickson3@gmail.com>
2015-03-29 19:41:38 -0700

    ot-site example

---
+++ src/ot-site.js
@@ -0,0 +1,12 @@
+import {Component, Template} from 'angular2/angular2';
+
+@Component({
+    selector: 'ot-site'  
+})
+@Template({
+  url: "ot-site.html"
+})
+export class OtSite {
+  constructor() {
+  }
+}
